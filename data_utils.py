c9892e7a65bbef3514efe646fefe9c26b2d4f130
partydog <waeechang@gmail.com>
2021-02-08 14:09:34 +0800

    Add files via upload

--- data_utils.py
+++ data_utils.py
@@ -250,11 +250,32 @@ class TrainDatasetFromFolder(Dataset):
                 data_orig = data_orig.resize((re_width, re_height))
                 width, height = data_orig.size
 
+        print('width' + str(width))
+        print('height' + str(height))
+        print('re_width' + str(re_width))
+        print('re_height' + str(re_height))
+
         data_yuv444, data_yuv420, data_rgb = bgr2yuv(data_orig, bkW, bkH)
+
+        print('data_yuv420:')
+        print(len(data_yuv420))
+        print('data_yuv444:')
+        print(len(data_yuv444))
+        print('data_rgb:')
+        print(len(data_rgb))
 
         list_tensor_yuv444 = return_image_block(data_yuv444)
         list_tensor_yuv420 = return_image_block(data_yuv420)
         list_tensor_rgb = return_image_block(data_rgb)
+
+        print('list_tensor_yuv420:')
+        print(len(list_tensor_yuv420))
+        print('list_tensor_yuv444:')
+        print(len(list_tensor_yuv444))
+        print('list_tensor_rgb:')
+        print(len(list_tensor_rgb))
+
+        input()
 
         if bTest:
             num_BK_Width = int(width/bkW)
@@ -266,16 +287,6 @@ class TrainDatasetFromFolder(Dataset):
             input()
             save_image(data_rgb, bkW, bkH, num_BK_Width, num_BK_Height, 'rgb', index)
             input()
-
-        '''
-        img_full = np.concatenate(imgs[0:w_bk_num], 1)
-        full_bk_num = w_bk_num * h_bk_num
-        for i in range(w_bk_num, full_bk_num, w_bk_num):
-            img_row = np.concatenate(imgs[i:i + w_bk_num], 1)
-            img_full = np.concatenate([img_full, img_row], 0)
-
-        imwrite("result_full_" + str(index) + ".jpg", img_full)
-        '''
 
         '''
         index = 0
